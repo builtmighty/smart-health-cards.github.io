d15311aee2eff4c6a7ffb7a85a8d2ae5b6fb0d99
Kreig Durham <kreig@builtmighty.com>
2024-09-04 17:36:08 +0000

    ♻️ Update devcontainer setup script and permissions
    Signed-off-by: GitHub <noreply@github.com>

--- .devcontainer/scripts/setup.sh
+++ .devcontainer/scripts/setup.sh
@@ -10,13 +10,25 @@ echo  "3. Serve the Jekyll site"
 echo  "8. Exit script with Success Code - 0"
 echo  "\n===============================================================================\n"
 
+# Globally set the SITE_HOST variable 🌐
+if [ -z ${CODESPACE_NAME+x} ]; then
+	export SITE_DOMAIN="localhost"
+	export SITE_HOST="localhost"
+else
+	export SITE_DOMAIN="${CODESPACE_NAME}-443.${GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN}"
+	export SITE_HOST="https://${CODESPACE_NAME}-443.${GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN}"
+fi
+
 # Update the CODESPACES-WELCOME-MESSAGE.md file with the Codespace URL but first Ignore those changes in Git 📩
-git update-index --assume-unchanged $DOCUMENT_ROOT/.devcontainer/CODESPACES-WELCOME-MESSAGE.md
-sed -i "s|{insert-codespace-url-here}|$SITE_HOST|" $DOCUMENT_ROOT/.devcontainer/CODESPACES-WELCOME-MESSAGE.md && \
+git update-index --assume-unchanged .devcontainer/CODESPACES-WELCOME-MESSAGE.md
+sed -i "s|{insert-codespace-url-here}|$SITE_HOST|" .devcontainer/CODESPACES-WELCOME-MESSAGE.md && \
 
 # Install the bundler from ruby gems
 echo "🔧 Installing the bundler from ruby gems"
 gem install bundler
+
+# Check for updates before building
+bundle update
 
 # Install required bundles from the Gemfile
 echo "🔧 Installing required bundles from the Gemfile"
@@ -24,4 +36,8 @@ bundle install --verbose
 
 # Serve the Jekyll site
 echo "🚀 Serving the Jekyll site"
-bundle exec jekyll serve --port 443
+bundle exec jekyll serve --host 0.0.0.0 --port 4000 --open-url --force_polling --drafts --trace --livereload
+
+# Mark DOCUMENT_ROOT and WEB_ROOT as Safe Directories for .git to access 🧑‍💻
+echo "\n🧑‍💻 Adding Safe Directories for .git to access\n"
+git config --global --add safe.directory ./
